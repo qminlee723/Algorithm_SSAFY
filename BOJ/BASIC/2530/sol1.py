0fcd6678c5f1f0746de6e93857e387ecf7b0a4bc
qminlee723 <gyumin.q.lee@gmail.com>
2022-03-13 22:35:52 +0900

    0313

--- BOJ/BASIC/2530/sol1.py
+++ BOJ/BASIC/2530/sol1.py
@@ -5,23 +5,31 @@ sys.stdin = open('input.txt')
 H, M, S = map(int, input().split())
 duration = int(input())
 
-if duration // 360 < 1:
-    if duration // 60 == 0:
-        M += duration // 60
-        S += duration % 60
+if duration // 3600 < 1:
+    if duration // 60 < 1:
+        S += duration
     else:
         M += duration // 60
-        S += duration % 60
+        S += (duration - (duration // 60) * 60) % 60
 else:
     H += duration // 3600
-    M += (duration - 3600*(duration // 3600)) // 60
+    M += (duration - 3600 * (duration // 3600)) // 60
     S += duration % 60
+
+# 작은 단위부터
+if S >= 60:
+    M += S // 60
+    S -= S // 60 * 60
 
 if M >= 60:
     H += M // 60
     M -= M // 60 * 60
-elif S >= 60:
-    M += S // 60
-    S -= S // 60 * 60
+
+if H >= 24:
+    H = 0
 
 print(H, M, S)
+
+H = H + (duration + M * 60 + S) // 3600
+M =
+print(H)
