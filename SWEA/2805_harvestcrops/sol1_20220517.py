76eca93c739d30218a84a2b4400017dfe7aecbfd
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-05-18 23:28:15 +0900

    0518

---
+++ SWEA/2805_harvestcrops/sol1_20220517.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    farm_size = int(input())
+    crop_value = list(map(int, input()))
+
+    for i in range(farm_size):
+
+    print(crop_value)
